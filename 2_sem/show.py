c190d5e006c00c78bba3dd2383e0f1aa425ca954
Violonur-PavelBI <pacha.znaet.vse@yandex.ru>
2023-06-16 14:13:10 +0300

    унифицировал форматирование(black)

--- 2_sem/show.py
+++ 2_sem/show.py
@@ -1,35 +1,36 @@
 # -*- coding: utf-8 -*-
 import matplotlib.pyplot as plt
 
-def show(mass,a,b,d = False):
+
+def show(mass, a, b, d=False):
     y = mass[1][a:b]
     x = mass[0][a:b]
-    c = [y[0]]*len(y)
+    c = [y[0]] * len(y)
     if d:
-        c = [d[1]]*len(y)
+        c = [d[1]] * len(y)
     fig = plt.figure()
     ax1 = fig.add_subplot(111)
-    ax1.plot(x,y, color='green')
-    ax1.plot(x,c)
-    ax1.set_xlabel(u'Epoch')
-    ax1.set_ylabel(u'Loss', color='green')
-    ax1.grid(True, color='green')
-    ax1.tick_params(axis='y', which='major', labelcolor='green')
-    ax1.set_title(u'Динамика Loss')
+    ax1.plot(x, y, color="green")
+    ax1.plot(x, c)
+    ax1.set_xlabel("Epoch")
+    ax1.set_ylabel("Loss", color="green")
+    ax1.grid(True, color="green")
+    ax1.tick_params(axis="y", which="major", labelcolor="green")
+    ax1.set_title("Динамика Loss")
     plt.show()
-    
+
     y = mass[2][a:b]
     x = mass[0][a:b]
-    c = [y[0]]*len(y)
+    c = [y[0]] * len(y)
     if d:
-        c = [d[0]]*len(y)
+        c = [d[0]] * len(y)
     fig = plt.figure()
     ax1 = fig.add_subplot(111)
-    ax1.plot(x,y, color='green')
-    ax1.plot(x,c)
-    ax1.set_xlabel(u'Epoch')
-    ax1.set_ylabel(u'accuracy', color='green')
-    ax1.grid(True, color='green')
-    ax1.tick_params(axis='y', which='major', labelcolor='green')
-    ax1.set_title(u'Динамика accuracy')
+    ax1.plot(x, y, color="green")
+    ax1.plot(x, c)
+    ax1.set_xlabel("Epoch")
+    ax1.set_ylabel("accuracy", color="green")
+    ax1.grid(True, color="green")
+    ax1.tick_params(axis="y", which="major", labelcolor="green")
+    ax1.set_title("Динамика accuracy")
     plt.show()
